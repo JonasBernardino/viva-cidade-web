b6920857b1600ebed5c0e47de43a22c8b276a9ea
jonasbernardino <jonasl.bernardino@gmail.com>
2025-04-23 11:12:52 -0300

    first commit

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isScrolled = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrolled = window.scrollY > 0;
+    this.isScrolled = scrolled;
+    console.log(this.isScrolled)
+  }
+  
 
 }
