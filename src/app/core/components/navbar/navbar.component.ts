59cb987eb898a37b590f2b059c61b12133bf5431
jonasbernardino <jonasl.bernardino@gmail.com>
2025-04-30 00:24:35 -0300

    adicionando menu hamburguer

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,21 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostListener } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   isScrolled = false;
+  menuAberto = false;
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    const scrolled = window.scrollY > 0;
-    this.isScrolled = scrolled;
-    console.log(this.isScrolled)
+    this.isScrolled = window.scrollY > 0;
   }
-  
 
+  toggleMenu() {
+    this.menuAberto = !this.menuAberto;
+  }
+
+  fecharMenu() {
+    this.menuAberto = false;
+  }
 }
